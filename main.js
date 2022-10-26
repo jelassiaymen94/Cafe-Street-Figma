cf9d97fd27aaca8efa866634367ca146ab59e4dc
Jelassi Aymen <nhedber1@students.solano.edu>
2022-10-26 11:01:29 +0100

    modal created

--- main.js
+++ main.js
@@ -68,3 +68,28 @@ specialCardTitle.forEach((title, index) => {
 for (const [key, value] of Object.entries(popularContentCardObject)) {
 	localStorage.setItem(key, value);
 }
+
+//* MODAL / POPUP *//
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+cartCount.onclick = function () {
+	modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
